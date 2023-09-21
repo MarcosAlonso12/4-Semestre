88a763ae3baa9dc48160649e84a125746e1b92eb
Marcos Alonso <marcosbrps4@gmail.com>
2023-09-20 21:49:53 -0400

    Aula 13-09

--- src/main/java/com/tads4/tads4/controllers/ProductController.java
+++ src/main/java/com/tads4/tads4/controllers/ProductController.java
@@ -1,15 +1,18 @@
 package com.tads4.tads4.controllers;
 
+import ch.qos.logback.core.util.ExecutorServiceUtil;
 import com.tads4.tads4.dto.ProductDTO;
 import com.tads4.tads4.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.web.config.ReactiveQuerydslWebConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,9 +25,11 @@ public class ProductController {
         return service.findById(id);
     }
 
-    @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable){
-        return service.findAll(pageable);
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getInd()).toUri();
+        return ResponseEntity.created(uri).body(dto);
 
     }
 }
