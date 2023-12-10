47cd67ca781a5460b2532262a1321cd283c928f2
“Marcos <marcosbrps4@gmail.com>
2023-12-10 11:28:56 -0400

    Finalizado

--- src/main/java/com/tads4/ecommerce/EcommerceApplication.java
+++ src/main/java/com/tads4/ecommerce/EcommerceApplication.java
@@ -1,11 +1,12 @@
 package com.tads4.ecommerce;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceApplication {
-
+    public String PORT = System.getenv("PORT");
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
